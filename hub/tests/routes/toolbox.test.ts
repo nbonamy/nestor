ee6374232fc636b7eae6e110a4d278c46a4f2ce5
Nicolas Bonamy <nicolas@bonamy.fr>
2024-09-27 10:54:36 -0500

    tests

--- hub/tests/routes/toolbox.test.ts
+++ hub/tests/routes/toolbox.test.ts
@@ -4,14 +4,10 @@ import supertest from 'supertest'
 import app from '../../src/server'
 import Marshaller from '../../src/services/marshaller'
 
-vi.mock('../../src/services/marshaller', () => {
-  const Marshaller = vi.fn()
-  Marshaller.prototype.toOpenAI = vi.fn()
-  return { default: Marshaller }
-})
+const spyOpenAI = vi.spyOn(Marshaller.prototype, 'toOpenAI')
 
 test('toolbox', async () => {
   const res = await supertest(app).get('/toolbox')
   expect(res.status).toBe(200)
-  expect(Marshaller.prototype.toOpenAI).toHaveBeenCalled()
+  expect(spyOpenAI).toHaveBeenCalled()
 })
--- hub/tests/unit/discovery.test.ts
+++ hub/tests/unit/discovery.test.ts
@@ -3,30 +3,42 @@ import { vi, test, expect } from 'vitest'
 import ServiceDiscovery from '../../src/services/discovery'
 import mdns from 'mdns'
 
-vi.mock('mdns', () => {
-  const browser = {
-    start : vi.fn(),
-    on: vi.fn()
+const spyCreateBrowser = vi.spyOn(mdns, 'createBrowser')
+
+class ServiceMock {
+  ad: any
+  start(port: number) {
+    this.ad = mdns.createAdvertisement(mdns.tcp('nestor', 'service'), port, {
+      name: 'Nestor Service',
+      txtRecord: {
+        type: 'service',
+      }
+    })
+    this.ad.start()
   }
-  return { default: {
-    tcp: vi.fn(),
-    createBrowser: vi.fn(() => browser)
-  }}
-})
+  stop() {
+    this.ad.stop()
+  }
+}
 
 test('creates browser', async () => {
   const discovery = new ServiceDiscovery()
-  expect(mdns.createBrowser).not.toHaveBeenCalled()
   discovery.start(() => {}, () => {})
-  expect(mdns.createBrowser).toHaveBeenCalled()
+  expect(spyCreateBrowser).toHaveBeenCalled()
 })
 
 test('calls callbacks', async () => {
-  const onUp = vi.fn(() => {})
-  const onDown = vi.fn(() => {})
+  let count = 0
+  const onUp = vi.fn(() => { count = count + 1 })
+  const onDown = vi.fn(() => { count = count - 1 })
   const discovery = new ServiceDiscovery()
   discovery.start(onUp, onDown)
-  expect(onUp).not.toHaveBeenCalled()
+  const serviceMock = new ServiceMock()
+  serviceMock.start(3000)
+  await vi.waitUntil(() => count > 0, 5000)
+  expect(onUp).toHaveBeenCalled()
   expect(onDown).not.toHaveBeenCalled()
-  //TODO
+  serviceMock.stop()
+  await vi.waitUntil(() => count == 0, 5000)
+  expect(onDown).toHaveBeenCalled()
 })
