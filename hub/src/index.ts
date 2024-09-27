188b5a3355ad2f91a6fc589d74bd3605843d2951
Nicolas Bonamy <nicolas@bonamy.fr>
2024-09-27 14:11:25 -0500

    hub options for name and port

--- hub/src/index.ts
+++ hub/src/index.ts
@@ -1,10 +1,28 @@
 
 import portfinder from 'portfinder'
+import { program, InvalidArgumentError } from 'commander'
 import * as mdns from 'mdns'
 import app from './server'
 
+function commanderParseInt(value: string) {
+  const parsedValue = parseInt(value, 10);
+  if (isNaN(parsedValue)) {
+    throw new InvalidArgumentError('Not a number.');
+  }
+  return parsedValue;
+}
+
+program
+  .version(require('../package.json').version, '-v, --version')
+  .usage('[OPTIONS]...')
+  .option('-n, --name <value>', 'Name of Hub on the network', 'Nestor Hub')
+  .option('-p, --port <value>', 'Port number', commanderParseInt, 3000)
+  .parse(process.argv);
+
+const options = program.opts();
+
 // now start it
-portfinder.getPort({ port: 3000 }, (err, port) => {
+portfinder.getPort({ port: options.port }, (err, port) => {
 
   if (err) {
     console.error('Error while finding port', err)
@@ -17,7 +35,7 @@ portfinder.getPort({ port: 3000 }, (err, port) => {
 
     // subtype is not consistently supported so using txtRecord too
     const ad = mdns.createAdvertisement(mdns.tcp('nestor', 'hub'), port, {
-      name: 'Nestor Hub',
+      name: options.name,
       txtRecord: {
         type: 'hub',
       }
