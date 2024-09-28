9068e7c21d7ee468e4942a2fd5212679f6060d1f
Nicolas Bonamy <nicolas@bonamy.fr>
2024-09-28 18:29:55 -0500

    pm2 test

---
+++ hub/ecosystem.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  apps : [{
+    name   : "nestor-hub",
+    script : "./dist/index.ts",
+    watch: true,
+		watch_delay: 1000,
+		ignore_watch : ["config.yml*"],
+    log_date_format: "YYYY-MM-DD HH:mm:ss"
+  }]
+}
